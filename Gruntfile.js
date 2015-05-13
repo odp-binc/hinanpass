a45b5dca5d8409454c5384ddbf9228ce8670696a
tsukamoto <tsukamoto@jig.jp>
2015-05-13 13:27:45 +0900

    auto upload add

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,32 +23,26 @@ module.exports = function (grunt) {
 		coffee: {
 			compile: {
 				files: {
-					'public_html/js/main.js': ['public_html/js/src/localize.coffee','public_html/js/src/*.coffee'] 
+					'public_html/dest/js/main.js': ['public_html/js/src/localize.coffee','public_html/js/src/*.coffee'] 
 				}
 			}
 		},
 		uglify: {
 			dist: {
 				files: {
-					'public_html/dest/js/main.min.js': 'public_html/js/main.js'
+					'public_html/dest/js/main.min.js': 'public_html/dest/js/main.js'
 				}
 			}
 		},
 		htmlmin: {
 			dist: {
 				files: {
-					'public_html/dest/index.html': 'public_html/index.html'
+					'public_html/dest/index.html': 'public_html/index.html',
+					'public_html/dest/test.html': 'public_html/test.html'
 				},
 				options: {
 					removeComments: true,
 					collapseWhitespace: true
-				}
-			}
-		},
-		copy: {
-			dist: {
-				files: {
-					'public_html/dest/images/**': 'public_html/**'
 				}
 			}
 		},
@@ -89,22 +83,101 @@ module.exports = function (grunt) {
 				]
 			}
 		},
+		sshexec: {
+			testup: {
+				command: 'bash ./testup.sh',
+				options: {
+					host: '54.64.95.43',
+					username: 'tsukamoto',
+					password: '',
+					privateKey: grunt.file.read("/Users/tsukamoto/.ssh/id_rsa")
+				}
+			}
+		},
+		sftp: {
+			html: {
+				files: {
+					"./": "public_html/dest/*"
+				},
+				options: {
+					path: "/home/tsukamoto/dest/",
+					host: "54.64.95.43",
+					username: 'tsukamoto',
+					password: "",
+					privateKey: grunt.file.read("/Users/tsukamoto/.ssh/id_rsa"),
+					srcBasePath: "public_html/dest/"
+
+				}
+			},
+			css: {
+				files: {
+					"./": "public_html/dest/css/*"
+				},
+				options: {
+					path: "/home/tsukamoto/dest/css/",
+					host: "54.64.95.43",
+					username: 'tsukamoto',
+					password: "",
+					privateKey: grunt.file.read("/Users/tsukamoto/.ssh/id_rsa"),
+					srcBasePath: "public_html/dest/css"
+				}
+			},
+			js: {
+				files: {
+					"./": "public_html/dest/js/*"
+				},
+				options: {
+					path: "/home/tsukamoto/dest/js/",
+					host: "54.64.95.43",
+					username: 'tsukamoto',
+					password: "",
+					privateKey: grunt.file.read("/Users/tsukamoto/.ssh/id_rsa"),
+					srcBasePath: "public_html/dest/js"
+				}
+			},
+			jslibs: {
+				files: {
+					"./": "public_html/dest/js/libs/*"
+				},
+				options: {
+					path: "/home/tsukamoto/dest/js/libs/",
+					host: "54.64.95.43",
+					username: 'tsukamoto',
+					password: "",
+					privateKey: grunt.file.read("/Users/tsukamoto/.ssh/id_rsa"),
+					srcBasePath: "public_html/dest/js/libs"
+				}
+			},
+			images: {
+				files: {
+					"./": "public_html/dest/images/*"
+				},
+				options: {
+					path: "/home/tsukamoto/dest/images/",
+					host: "54.64.95.43",
+					username: 'tsukamoto',
+					password: "",
+					privateKey: grunt.file.read("/Users/tsukamoto/.ssh/id_rsa"),
+					srcBasePath: "public_html/dest/images"
+				}
+			}
+		},
 		watch: {
 			sass: {
 				files: ["public_html/scss/*.scss"],
-				tasks: ['sass', 'cssmin']
+				tasks: ['sass', 'cssmin', 'sftp:css', 'sshexec']
 			},
 			js: {
 				files: 'public_html/js/src/*.coffee',
-				tasks: ['coffee', 'uglify', 'yuidoc']
+				tasks: ['coffee', 'uglify', 'sftp:js', 'sshexec']
 			},
 			html: {
 				files: 'public_html/*.html',
-				tasks: ['htmlmin']
+				tasks: ['htmlmin', 'sftp:html', 'sshexec']
 			},
 			image: {
 				files: 'public_html/images/**',
-				tasks: ['copy']
+				tasks: ['copy', 'sftp:images', 'sshexec']
 			},
 			options: {
 				livereload: true
@@ -121,5 +194,6 @@ grunt.loadNpmTasks('grunt-contrib-htmlmin');
 grunt.loadNpmTasks('grunt-contrib-connect');
 grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.registerTask('default', ['coffee', 'uglify', 'sass', 'cssmin', 'htmlmin', 'copy', 'connect', 'watch']);
+grunt.loadNpmTasks('grunt-ssh');
+grunt.registerTask('default', ['coffee', 'uglify', 'sass', 'cssmin', 'htmlmin', 'copy', 'connect', 'sftp', 'sshexec', 'watch']);
 };
